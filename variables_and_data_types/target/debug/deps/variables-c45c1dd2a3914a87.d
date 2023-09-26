eaec2eae2913d97e6cd2d80dc41298d0cc325bed
minhdang118 <mdang2@u.rochester.edu>
2023-09-26 19:01:57 -0400

    no4

--- variables_and_data_types/target/debug/deps/variables-c45c1dd2a3914a87.d
+++
@@ -1,5 +0,0 @@
-/home/minhdang118/rust_learn_local/variables_and_data_types/target/debug/deps/variables-c45c1dd2a3914a87.rmeta: src/main.rs
-
-/home/minhdang118/rust_learn_local/variables_and_data_types/target/debug/deps/variables-c45c1dd2a3914a87.d: src/main.rs
-
-src/main.rs:
--- variables_and_data_types/target/debug/deps/variables-f68db27a9254211c.d
+++
@@ -1,5 +0,0 @@
-/home/minhdang118/rust_learn_local/variables_and_data_types/target/debug/deps/variables-f68db27a9254211c.rmeta: src/main.rs
-
-/home/minhdang118/rust_learn_local/variables_and_data_types/target/debug/deps/variables-f68db27a9254211c.d: src/main.rs
-
-src/main.rs:
