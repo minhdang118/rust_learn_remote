5cb2ae1de5a0666a43aa3d415e701a420075a237
minhdang118 <mdang2@u.rochester.edu>
2023-09-26 17:47:52 -0400

    update functions

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,8 +1,23 @@
 fn main() {
-    println!("Hello, world!");
-    another_fn();
+    // println!("Hello, world!");
+    another_fn(8, 'h');
+
+    let y = {
+        let x = 3;
+        x + 1
+    };
+
+    println!("The value of y is: {y}");
+
+    let x = mul_2(7);
+
+    println!("The value of x is: {x}");
 }
 
-fn another_fn() {
-    println!("From another function!")
+fn another_fn(x: i32, l: char) {
+    println!("Parameter as a string is {x}{l}");
+}
+
+fn mul_2(x: i32) -> i32 {
+    x * 2
 }
